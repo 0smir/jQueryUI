2e1304e6dd41cd663d6f8ae753d1f25ba7b0b224
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-08-25 00:52:18 +0300

    add animation duration for inputDatapucker, add multiple showin monthes (3), add styles for multiple showing monthes

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,18 @@ $(document).ready(function () {
         showOtherMonths: true,
         selectOtherMonths: true,
         showAnim: "clip",
+        duration: "slow",
         showButtonPanel: true
     });
     staticCalendar.datepicker({
         showOtherMonths: true,
-        selectOtherMonths: true
+        selectOtherMonths: true,
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 3
     });
+
+    staticCalendar.find(".ui-datepicker-inline").css({"display":"flex",
+                                                    "flex-direction":"column",
+                                                    "width":"220px"});
 });
