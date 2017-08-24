4de780dc29a26402dbe28b97613d60fd99ce04bf
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-08-25 00:27:12 +0300

    datepicker embedded in the page instead of in an overlay

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     var simpleDateInput = $(".input-simple-date");
-
+    var staticCalendar = $(".static-calendar");
     simpleDateInput.datepicker({
         showOtherMonths: true,
         selectOtherMonths: true,
         showAnim: "clip",
         showButtonPanel: true
-
+    });
+    staticCalendar.datepicker({
+        showOtherMonths: true,
+        selectOtherMonths: true
     });
 });
