19a9c9d71be24fbe237c9b24472c128eb80c3711
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-08-24 23:51:09 +0300

    add for datapicker options: animation, show and select date from other month(in same div)

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     var simpleDateInput = $(".input-simple-date");
+
     simpleDateInput.datepicker({
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        showAnim: "clip"
 
     });
-
 });
