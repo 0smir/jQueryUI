737b3c6afeb84fb3d4b041c9eb40efb685b46811
Olga Smirnova <o.v.smirnova1987@gmail.com>
2017-08-26 13:24:53 +0300

    add regional data (month and days naming)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
     var simpleDateInput = $(".input-simple-date");
     var staticCalendar = $(".static-calendar");
+
+    $.datepicker.regional.ru = {
+        monthNames: ['Январь','Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Янв','Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Ноя', 'Дек'],
+        dayNames: [ "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" ],
+        dayNamesMin: [ "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ]
+
+    };
+    $.datepicker.setDefaults($.datepicker.regional.ru );
+
+
+
     simpleDateInput.datepicker({
         showOtherMonths: true,
         selectOtherMonths: true,
@@ -8,6 +20,7 @@ $(document).ready(function () {
         duration: "slow",
         showButtonPanel: true,
         dateFormat: "dd-mm-yy"
+
     });
     simpleDateInput.datepicker("setDate",  new Date());
     staticCalendar.datepicker({
@@ -15,10 +28,15 @@ $(document).ready(function () {
         selectOtherMonths: true,
         changeMonth: true,
         changeYear: true,
+        dateFormat: "dd-mm-yy",
+        showMonthAfterYear: false,
         numberOfMonths: 3
     });
 
     staticCalendar.find(".ui-datepicker-inline").css({"display":"flex",
-                                                    "flex-direction":"column",
-                                                    "width":"220px"});
+        "flex-direction":"column",
+        "width":"220px"});
+
+
+
 });
